ffb5387c1e328af524bcd3679ef831dba2e715ff
vishnu <vishnu.m@pacewisdom.com>
2022-08-04 00:26:07 +0530

    fileuploads done

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import json
 import os.path
-from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
+app.secret_key = 'djbadbbhh1b32h3b2bsndsjjdns32jn2bh3h2'
 
 @app.route('/')
 def home():
@@ -17,9 +19,17 @@ def your_url():
                 urls = json.load(urls_file)
               
         if request.form['code'] in urls.keys(): 
+            flash('That shortname has already been taken. Please select another name')
             return redirect(url_for('home'))
         
-        urls[request.form['code']] = {'url':request.form['url']}
+        if 'url' in request.form.keys():
+            urls[request.form['code']] = {'url':request.form['url']}
+        else:
+            f = request.files['file']
+            full_name = request.form['code'] + secure_filename(f.filename)
+            f.save('/Users/Admin/Documents/flask exercise files/Ex_Files_Flask_EssT/url-shortner/'+full_name)    
+            urls[request.form['code']] = {'file':full_name}
+        
         with open('urls.json','w') as url_file:
             json.dump(urls, url_file)
         return render_template('your_url.html', code = request.form['code'])
