995d349f03684d9fa9bcef9e6f30e7e27d88650f
vishnu <vishnu.m@pacewisdom.com>
2022-08-06 01:05:46 +0530

    creating api

--- app.py
+++ app.py
@@ -2,14 +2,14 @@ from fileinput import filename
 import json
 import os.path
 from werkzeug.utils import secure_filename
-from flask import Flask, render_template, request, redirect, url_for, flash, abort, session
+from flask import Flask, render_template, request, redirect, url_for, flash, abort, session, jsonify
 
 app = Flask(__name__)
 app.secret_key = 'djbadbbhh1b32h3b2bsndsjjdns32jn2bh3h2'
 
 @app.route('/')
 def home():
-    return render_template('home.html', name='Vishnu')
+    return render_template('home.html', codes=session.keys())
 
 @app.route('/your-url',methods=[ 'GET','POST'])
 def your_url():
@@ -54,3 +54,7 @@ def redirect_to_url(code):
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('page_not_found.html'),404
+
+@app.route('/api')
+def session_api():
+    return jsonify(list(session.keys()))
